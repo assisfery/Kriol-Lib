2d3be67f0c128fa6e6d11c7723ab282f269c7cea
Assis Ferreira <assisrunny@hotmail.com>
2017-11-18 22:08:04 -0100

    Updating gh-pages from master branch via GitBot

---
+++ kriol-lib.h
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+
+/*
+    CRIOU AQUI A
+    POSSIBILIDADE DE PROGRAMAR
+    EM C, EM CRIOULO :D
+
+    KRIOL-LIB.h v1.0
+    by Assis Ferreira
+*/
+
+// PALAVRAS RESERVADAS
+
+#define timenti while
+#define fazi do
+#define si if
+#define para for
+#define skodji switch
+#define menu main
+
+// TIPOS DE DADOS
+
+#define interu int
+#define decimal float
+#define letra char
+#define palavra char*
+#define vaziu void
+
+#define verdadi 1
+#define falsu 0
+
+// ENTRADA E SAIDA DE DADOS
+
+#define imprimi printf
+#define recebi scanf
---
+++ main.c
@@ -0,0 +1,14 @@
+#include "kriol-lib.h"
+
+void main()
+{
+    interu x = 1;
+
+    fazi
+    {
+        imprimi("Valor de x: %d\n", x);
+        x++;
+
+    }timenti(x < 12);
+
+}
